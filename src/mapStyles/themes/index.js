61f5304648a5467d720dd4873504b749978ae8c5
Kamigami55 <eason@easonchang.com>
2021-11-09 00:44:39 +0800

    Add more map themes

--- src/mapStyles/themes/index.js
+++ src/mapStyles/themes/index.js
@@ -1,11 +1,17 @@
+import ThemeAubergine from './ThemeAubergine'
+import ThemeDark from './ThemeDark'
 import ThemeNight from './ThemeNight'
 import ThemeRetro from './ThemeRetro'
+import ThemeSilver from './ThemeSilver'
 import ThemeStandard from './ThemeStandard'
 
 const MapThemes = [
   { id: '1', name: '標準', config: ThemeStandard, imageSrc: '/images/mapTheme/standard.png' },
   { id: '2', name: '懷舊', config: ThemeRetro, imageSrc: '/images/mapTheme/retro.png' },
   { id: '3', name: '深夜', config: ThemeNight, imageSrc: '/images/mapTheme/night.png' },
+  { id: '4', name: '暗黑', config: ThemeDark, imageSrc: '/images/mapTheme/dark.png' },
+  { id: '5', name: '銀白', config: ThemeSilver, imageSrc: '/images/mapTheme/silver.png' },
+  { id: '6', name: '深藍', config: ThemeAubergine, imageSrc: '/images/mapTheme/aubergine.png' },
 ]
 
 export default MapThemes
