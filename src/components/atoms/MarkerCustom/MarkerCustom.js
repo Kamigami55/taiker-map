c9f23631bd20d8879f6270a648c3ef602383aafa
Kamigami55 <eason@easonchang.com>
2021-11-12 03:06:54 +0800

    Add circle shape marker style

--- src/components/atoms/MarkerCustom/MarkerCustom.js
+++ src/components/atoms/MarkerCustom/MarkerCustom.js
@@ -1,17 +1,17 @@
 import PropTypes from 'prop-types'
 import { Marker } from '@react-google-maps/api'
 
-export default function MarkerCustom({ spot }) {
+import { MARKER_SHAPE_CIRCLE, MARKER_SHAPE_CONFIGS, MARKER_SHAPE_PIN } from './markerShape'
+
+export default function MarkerCustom({ spot, shape }) {
+  const ShapeConfig = MARKER_SHAPE_CONFIGS[shape]
   return (
     <Marker
       position={spot.position}
       icon={{
-        path: 'M25.015 2.4c-7.8 0-14.121 6.204-14.121 13.854 0 7.652 14.121 32.746 14.121 32.746s14.122-25.094 14.122-32.746c0-7.65-6.325-13.854-14.122-13.854z',
-        anchor: { x: 25, y: 50 },
-        labelOrigin: { x: 25, y: 19 },
+        ...ShapeConfig,
         fillColor: 'red',
         fillOpacity: 1,
-        scale: 0.8,
         strokeColor: 'black',
         strokeWeight: 1,
       }}
@@ -29,4 +29,10 @@ MarkerCustom.propTypes = {
   spot: PropTypes.shape({
     position: PropTypes.shape({ lat: PropTypes.number, lng: PropTypes.number }),
   }).isRequired,
+  shape: PropTypes.oneOf([MARKER_SHAPE_PIN, MARKER_SHAPE_CIRCLE]),
+}
+
+MarkerCustom.defaultProps = {
+  // shape: MARKER_SHAPE_PIN,
+  shape: MARKER_SHAPE_CIRCLE,
 }
---
+++ src/components/atoms/MarkerCustom/markerShape.js
@@ -0,0 +1,17 @@
+export const MARKER_SHAPE_PIN = 'MARKER_SHAPE_PIN'
+export const MARKER_SHAPE_CIRCLE = 'MARKER_SHAPE_CIRCLE'
+
+export const MARKER_SHAPE_CONFIGS = {
+  [MARKER_SHAPE_PIN]: {
+    path: 'M25.015 2.4c-7.8 0-14.121 6.204-14.121 13.854 0 7.652 14.121 32.746 14.121 32.746s14.122-25.094 14.122-32.746c0-7.65-6.325-13.854-14.122-13.854z',
+    anchor: { x: 25, y: 50 },
+    labelOrigin: { x: 25, y: 19 },
+    scale: 0.8,
+  },
+  [MARKER_SHAPE_CIRCLE]: {
+    path: 'M 25 25 m -15, 0 a 15,15 0 1,0 30,0 a 15,15 0 1,0 -30,0',
+    anchor: { x: 25, y: 25 },
+    labelOrigin: { x: 25, y: 25 },
+    scale: 0.8,
+  },
+}
