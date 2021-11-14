53a0e1d238b68b582779dd81b2db59ebc19252ab
Kamigami55 <eason@easonchang.com>
2021-11-15 02:07:23 +0800

    Update showcase maps

--- src/components/organisms/ShowcaseSection/ShowcaseSection.js
+++ src/components/organisms/ShowcaseSection/ShowcaseSection.js
@@ -4,12 +4,10 @@ import Link from 'next/link'
 import Section from '@/components/molecules/Section'
 
 const ShowcaseImages = [
-  { id: '1', name: '標準', imageSrc: '/images/mapTheme/standard.png' },
-  { id: '2', name: '懷舊', imageSrc: '/images/mapTheme/retro.png' },
-  { id: '3', name: '深夜', imageSrc: '/images/mapTheme/night.png' },
-  { id: '4', name: '暗黑', imageSrc: '/images/mapTheme/dark.png' },
-  { id: '5', name: '銀白', imageSrc: '/images/mapTheme/silver.png' },
-  { id: '6', name: '深藍', imageSrc: '/images/mapTheme/aubergine.png' },
+  { id: '1', name: '宜蘭一日遊地圖', imageSrc: '/images/sampleMaps/yilan.png' },
+  { id: '2', name: '蘭嶼景觀地圖', imageSrc: '/images/sampleMaps/lanyu.png' },
+  { id: '3', name: '金門星光節住宿地圖', imageSrc: '/images/sampleMaps/kinmen.png' },
+  { id: '4', name: '基隆美食地圖', imageSrc: '/images/sampleMaps/keelong.png' },
 ]
 
 export default function ShowcaseSection() {
@@ -18,14 +16,19 @@ export default function ShowcaseSection() {
       <h2 className="mb-16 text-xl font-bold text-172A50">地圖展示</h2>
       <div className="flex overflow-x-scroll gap-[72px] items-center pb-4 mb-16">
         {ShowcaseImages.map(({ id, name, imageSrc }) => (
-          <div key={id} className="relative flex-shrink-0 w-[300px] h-[240px]">
-            <Image
-              src={imageSrc}
-              alt={name}
-              layout="fill"
-              objectFit="cover"
-              className="rounded-[56px]"
-            />
+          <div key={id} className="flex relative flex-col flex-shrink-0 gap-4 items-center">
+            <a href={imageSrc} target="_blank" rel="noopener noreferrer">
+              <div className="relative w-[300px] h-[240px]">
+                <Image
+                  src={imageSrc}
+                  alt={name}
+                  layout="fill"
+                  objectFit="cover"
+                  className="rounded-[56px]"
+                />
+              </div>
+            </a>
+            <p className="text-172A50">{name}</p>
           </div>
         ))}
       </div>
