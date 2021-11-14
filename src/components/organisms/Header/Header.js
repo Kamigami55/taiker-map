11828783c08ac96eedae50b3ccb5567ef70a60fd
Kamigami55 <eason@easonchang.com>
2021-11-14 19:54:32 +0800

    Add home page link to Header logo

--- src/components/organisms/Header/Header.js
+++ src/components/organisms/Header/Header.js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import Navigation from '@/components/molecules/Navigation'
 
 import { IoMdMap } from 'react-icons/io'
@@ -5,10 +7,12 @@ import { IoMdMap } from 'react-icons/io'
 export default function Header() {
   return (
     <header className="flex justify-between items-center py-3 px-10 w-full bg-white">
-      <div className="flex flex-row gap-2 items-center text-172A50">
-        <IoMdMap className="w-[40px] h-[40px]" />
-        <span className="ml-1 text-2xl font-bold leading-10 text-1A1A1A">Maper</span>
-      </div>
+      <Link href="/">
+        <a className="flex flex-row gap-2 items-center text-172A50">
+          <IoMdMap className="w-[40px] h-[40px]" />
+          <span className="ml-1 text-2xl font-bold leading-10 text-1A1A1A">Maper</span>
+        </a>
+      </Link>
 
       <Navigation />
     </header>
